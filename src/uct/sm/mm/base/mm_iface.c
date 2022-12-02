1fc8c4808168e40576eb8ff2a703263e329540aa
Brian Sheng <bins@nvidia.com>
2022-12-02 05:54:21 +0200

    UCT/SHM: Implement uct_iface_is_reachable_v2 in shm

--- src/uct/sm/mm/base/mm_iface.c
+++ src/uct/sm/mm/base/mm_iface.c
@@ -105,21 +105,33 @@ uct_mm_iface_query_tl_devices(uct_md_h md,
 }
 
 static int
-uct_mm_iface_is_reachable(const uct_iface_h tl_iface,
-                          const uct_device_addr_t *dev_addr,
-                          const uct_iface_addr_t *tl_iface_addr)
+uct_mm_iface_is_reachable_v2(const uct_iface_h tl_iface,
+                             const uct_iface_is_reachable_params_t *params)
 {
-    uct_mm_iface_t      *iface      = ucs_derived_of(tl_iface, uct_mm_iface_t);
-    uct_mm_md_t         *md         = ucs_derived_of(iface->super.super.md,
-                                                     uct_mm_md_t);
-    uct_mm_iface_addr_t *iface_addr = (void*)tl_iface_addr;
+    const uct_iface_addr_t *tl_iface_addr = params->iface_addr;
+    uct_mm_iface_t *iface                 = ucs_derived_of(tl_iface, uct_mm_iface_t);
+    uct_mm_md_t *md                       = ucs_derived_of(iface->super.super.md,
+                                                           uct_mm_md_t);
+    uct_mm_iface_addr_t *iface_addr       = (void*)tl_iface_addr;
 
-    if (!uct_sm_iface_is_reachable(tl_iface, dev_addr, tl_iface_addr)) {
+    if (!uct_sm_iface_is_reachable_v2(tl_iface, params)) {
         return 0;
     }
 
     return uct_mm_md_mapper_ops(md)->is_reachable(md, iface_addr->fifo_seg_id,
                                                   iface_addr + 1);
+}
+
+static int
+uct_mm_iface_is_reachable(const uct_iface_h tl_iface,
+                          const uct_device_addr_t *dev_addr,
+                          const uct_iface_addr_t *tl_iface_addr)
+{
+    return uct_iface_is_reachable_v2_wrapper(tl_iface,
+                                             dev_addr,
+                                             tl_iface_addr,
+                                             (uct_iface_is_reachable_v2_func_t)
+                                             uct_mm_iface_is_reachable_v2);
 }
 
 void uct_mm_iface_release_desc(uct_recv_desc_t *self, void *desc)
@@ -577,7 +589,7 @@ static uct_iface_internal_ops_t uct_mm_iface_internal_ops = {
     .ep_query              = (uct_ep_query_func_t)ucs_empty_function,
     .ep_invalidate         = (uct_ep_invalidate_func_t)ucs_empty_function_return_unsupported,
     .ep_connect_to_ep_v2   = ucs_empty_function_return_unsupported,
-    .iface_is_reachable_v2 = (uct_iface_is_reachable_v2_func_t)ucs_empty_function_return_unsupported
+    .iface_is_reachable_v2 = (uct_iface_is_reachable_v2_func_t)uct_mm_iface_is_reachable_v2
 };
 
 static void uct_mm_iface_recv_desc_init(uct_iface_h tl_iface, void *obj,
